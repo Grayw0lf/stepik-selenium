a4bdcfbae3bac7202ff540ef59194205f4ff4fed
Graywolf <akosheev@rambler.ru>
2019-12-13 14:49:44 +0300

    Change test_items.py, add message (3.6.9)

--- 3_6_9/test_items.py
+++ 3_6_9/test_items.py
@@ -8,4 +8,4 @@ def test_language_transmission(browser):
     browser.get(link)
     time.sleep(30)
     add_to_basket = browser.find_element(By.CSS_SELECTOR, '#add_to_basket_form button.btn-add-to-basket')
-    assert add_to_basket
+    assert add_to_basket, 'Button "Add to basket" not found.'
